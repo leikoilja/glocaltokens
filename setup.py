a8aedd5468cdc6e2d5ab6bdd6e23f0b2f498894d
Ilja Leiko <leikoilja@gmail.com>
2021-02-16 14:14:41 +0100

    Updating setup.py for the new release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("HISTORY.md") as history_file:
 
 setup_args = dict(
     name="glocaltokens",
-    version="0.1.4",
+    version="0.2.0",
     description="Tool to extract Google device local authentication tokens in Python",
     long_description_content_type="text/markdown",
     long_description=README + "\n\n" + HISTORY,
@@ -17,13 +17,16 @@ setup_args = dict(
     include_package_data=True,
     author="Ilja Leiko",
     author_email="leikoilja@gmail.com",
-    keywords=["Google", "Authentication", "LocalAuthenticationTokens"],
+    keywords=["Google", "Google Home", "Authentication", "LocalAuthenticationTokens"],
     url="https://github.com/leikoilja/glocaltokens",
     download_url="https://pypi.org/project/glocaltokens/",
 )
 
 install_requires = [
+    "uuid",
     "gpsoauth",
+    # grpcio is pinned by HomeAssistant
+    # See https://github.com/home-assistant/core/blob/6e67b943da09a13409e1a61ec34ed7144e1093c3/script/gen_requirements_all.py
     "grpcio==1.31.0",
     "grpcio-tools",
     "simplejson",
