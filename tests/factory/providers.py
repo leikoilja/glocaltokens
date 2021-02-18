cb14b418fd587d3e874d04f4eef5f165e50e3d74
Arnau Mora <arnyminer.z@gmail.com>
2021-02-18 16:39:57 +0100

    Started using `MASTER_TOKEN_LENGTH`, `ACCESS_TOKEN_LENGTH` and `LOCAL_AUTH_TOKEN_LENGTH` for `TokenProvider`.

--- tests/factory/providers.py
+++ tests/factory/providers.py
@@ -1,20 +1,28 @@
 from faker import Faker
 from faker.providers import BaseProvider
 
+from glocaltokens.const import (
+    ACCESS_TOKEN_LENGTH,
+    LOCAL_AUTH_TOKEN_LENGTH,
+    MASTER_TOKEN_LENGTH,
+)
 from glocaltokens.utils.token import generate as generate_token
+from glocaltokens.utils.type import Struct
+
+from .const import SMART_HOME_DEVICE_TYPES
 
 fake = Faker()
 
 
 class TokenProvider(BaseProvider):
     def master_token(self):
-        return generate_token(216, prefix="aas_et/")
+        return generate_token(MASTER_TOKEN_LENGTH, prefix="aas_et/")
 
     def access_token(self):
-        return generate_token(315, prefix="ya29.")
+        return generate_token(ACCESS_TOKEN_LENGTH, prefix="ya29.")
 
     def local_auth_token(self):
-        return generate_token(108)
+        return generate_token(LOCAL_AUTH_TOKEN_LENGTH)
 
 
 class HomegraphProvider(BaseProvider):
